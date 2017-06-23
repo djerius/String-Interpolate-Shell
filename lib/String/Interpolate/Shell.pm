fc5c12b96ac87c7ba5014b0bab6b63f50d22ba0d
Diab Jerius <djerius@cfa.harvard.edu>
2017-06-23 14:40:59 -0400

    switch to Dist::Zilla

--- lib/String/Interpolate/Shell.pm
+++ lib/String/Interpolate/Shell.pm
@@ -1,25 +1,6 @@
-# --8<--8<--8<--8<--
-#
-# Copyright (C) 2011 Smithsonian Astrophysical Observatory
-#
-# This file is part of String-Interpolate-Shell
-#
-# String-Interpolate-Shell is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# -->8-->8-->8-->8--
-
 package String::Interpolate::Shell;
+
+# ABSTRACT: Variable interpolation, shell style
 
 use strict;
 use warnings;
@@ -28,22 +9,22 @@ use Text::Balanced qw[ extract_bracketed extract_multiple extract_quotelike];
 use Params::Check qw[ check ];
 use Carp;
 
-use base 'Exporter';
+use Exporter 'import';
 
 our @EXPORT_OK = qw[ strinterp ];
 
-our $VERSION = 0.01;
+our $VERSION = '0.01';
 
 sub _extract {
 
     extract_multiple( $_[0],
-		      [
-		       qr/\s+/,
-		       qr/\\(\\)/,
-		       qr/\\(\$)/,
-		       { V => qr/\$(\w+)/ },
-		       { B => sub { (extract_bracketed( $_[0], '{}', qr/\$/ ))[0] } },
-		      ] );
+                      [
+                       qr/\s+/,
+                       qr/\\(\\)/,
+                       qr/\\(\$)/,
+                       { V => qr/\$(\w+)/ },
+                       { B => sub { (extract_bracketed( $_[0], '{}', qr/\$/ ))[0] } },
+                      ] );
 }
 
 sub _handle_undef {
@@ -69,123 +50,123 @@ sub strinterp{
 
     ## no critic(ProhibitAccessOfPrivateData)
     $attr = check( {
-		    undef_value => { allow => [ qw[ ignore remove ] ],
-				     default => 'ignore' },
-		    undef_verbosity => { allow => [ qw[ silent warn fatal ] ],
-					 default => 'silent' },
-		    undef_message => { default => "undefined variable: %s\n" },
-		    }, $attr || {} )
+                    undef_value => { allow => [ qw[ ignore remove ] ],
+                                     default => 'ignore' },
+                    undef_verbosity => { allow => [ qw[ silent warn fatal ] ],
+                                         default => 'silent' },
+                    undef_message => { default => "undefined variable: %s\n" },
+                    }, $attr || {} )
       or croak( "error parsing arguments: ", Params::Check::last_error() );
 
     my @matches;
 
     for my $matchstr ( _extract($text ) ) {
 
-	my $ref = ref $matchstr;
+        my $ref = ref $matchstr;
 
-	if ( 'B' eq $ref ) {
+        if ( 'B' eq $ref ) {
 
-	    # remove enclosing brackets
-	    my $match = substr( $$matchstr, 1,-1 );
+            # remove enclosing brackets
+            my $match = substr( $$matchstr, 1,-1 );
 
-	    # see if there's a 'shell' modifier expression
-	    my ( $ind, $q, $modf, $rest ) = $match =~ /^(!)?(\w+)(:[-?=+~:])?(.*)/;
+            # see if there's a 'shell' modifier expression
+            my ( $ind, $q, $modf, $rest ) = $match =~ /^(!)?(\w+)(:[-?=+~:])?(.*)/;
 
-	    # if there's no modifier but there is trailing cruft, it's an error
-	    die( "unrecognizeable variable name: \${$match}\n")
-		if ! defined $modf && $rest ne '';
+            # if there's no modifier but there is trailing cruft, it's an error
+            die( "unrecognizeable variable name: \${$match}\n")
+                if ! defined $modf && $rest ne '';
 
-	    # if indirect flag is set, expand variable
-	    if ( defined $ind ) {
+            # if indirect flag is set, expand variable
+            if ( defined $ind ) {
 
-		if ( defined $var->{$q} ) {
-		    $q = $var->{$q};
-		}
-		else
-		{
-		    push @matches, _handle_undef( $attr, '$' . $$matchstr );
-		    next;
-		}
-	    }
-
-
-	    if ( ! defined $modf ) {
-
-		push @matches, _handle_undef( $var, $q, $attr, '$' . $$matchstr );
-	    }
-
-	    elsif ( ':?' eq $modf ) {
-
-		local $attr->{undef_verbosity} = 'fatal';
-		local $attr->{undef_message} = $rest;
-
-		push @matches, _handle_undef( $var, $q, $attr, '$' . $$matchstr );
-
-	    }
-	    elsif ( ':-' eq $modf ) {
-
-		push @matches, defined $var->{$q} ? $var->{$q} : strinterp( $rest, $var, $attr );
-
-	    }
-
-	    elsif ( ':=' eq $modf ) {
+                if ( defined $var->{$q} ) {
+                    $q = $var->{$q};
+                }
+                else
+                {
+                    push @matches, _handle_undef( $attr, '$' . $$matchstr );
+                    next;
+                }
+            }
 
 
-		$var->{$q} = strinterp( $rest, $var, $attr )
-		  unless defined $var->{$q};
-		push @matches, $var->{$q};
+            if ( ! defined $modf ) {
 
-	    }
+                push @matches, _handle_undef( $var, $q, $attr, '$' . $$matchstr );
+            }
 
-	    elsif ( ':+' eq $modf ) {
+            elsif ( ':?' eq $modf ) {
 
-		push @matches, strinterp( $rest, $var, $attr )
-		  if defined $var->{$q};
+                local $attr->{undef_verbosity} = 'fatal';
+                local $attr->{undef_message} = $rest;
 
-	    }
+                push @matches, _handle_undef( $var, $q, $attr, '$' . $$matchstr );
 
-	    elsif ( '::' eq $modf ) {
+            }
+            elsif ( ':-' eq $modf ) {
 
-		push @matches, sprintf( $rest,  _handle_undef( $var, $q, $attr, '$' . $$matchstr ) );
+                push @matches, defined $var->{$q} ? $var->{$q} : strinterp( $rest, $var, $attr );
 
-	    }
+            }
 
-	    elsif ( ':~' eq $modf ) {
+            elsif ( ':=' eq $modf ) {
 
-		my ( $expr, $xtra, $op ) = (extract_quotelike( $rest ))[0,1,3];
-		die( "unable to parse variable substitution command: $rest\n" )
-		    if $xtra !~ /^\s*$/ or $op !~ /^(s|tr|y)$/;
 
-		my $t = $var->{$q};
-		## no critic(ProhibitStringyEval)
-		eval "\$t =~ $expr";
-		die $@ if $@;
+                $var->{$q} = strinterp( $rest, $var, $attr )
+                  unless defined $var->{$q};
+                push @matches, $var->{$q};
 
-		push @matches, $t;
+            }
 
-	    }
+            elsif ( ':+' eq $modf ) {
 
-	    else { die( "internal error" ) }
+                push @matches, strinterp( $rest, $var, $attr )
+                  if defined $var->{$q};
 
-	}
-	elsif ( 'V' eq $ref ) {
+            }
 
-	    my $q = $$matchstr;
+            elsif ( '::' eq $modf ) {
 
-	    push @matches, _handle_undef( $var, $q, $attr, "\$$q" );
+                push @matches, sprintf( $rest,  _handle_undef( $var, $q, $attr, '$' . $$matchstr ) );
 
-	}
+            }
 
-	elsif ( $ref ) {
+            elsif ( ':~' eq $modf ) {
 
-	    push @matches, $$matchstr;
+                my ( $expr, $xtra, $op ) = (extract_quotelike( $rest ))[0,1,3];
+                die( "unable to parse variable substitution command: $rest\n" )
+                    if $xtra !~ /^\s*$/ or $op !~ /^(s|tr|y)$/;
 
-	}
-	else {
+                my $t = $var->{$q};
+                ## no critic(ProhibitStringyEval)
+                eval "\$t =~ $expr";
+                die $@ if $@;
 
-	    push @matches, $matchstr;
+                push @matches, $t;
 
-	}
+            }
+
+            else { die( "internal error" ) }
+
+        }
+        elsif ( 'V' eq $ref ) {
+
+            my $q = $$matchstr;
+
+            push @matches, _handle_undef( $var, $q, $attr, "\$$q" );
+
+        }
+
+        elsif ( $ref ) {
+
+            push @matches, $$matchstr;
+
+        }
+        else {
+
+            push @matches, $matchstr;
+
+        }
 
 
     }
@@ -195,12 +176,9 @@ sub strinterp{
 }
 
 1;
+# COPYRIGHT
 
 __END__
-
-=head1 NAME
-
-String::Interpolate::Shell -- Variable interpolation, shell style
 
 =head1 SYNOPSIS
 
@@ -320,34 +298,9 @@ A message is output via C<croak()>.
 
 =back
 
-
-
 =back
 
 =back
 
 =head1 SEE ALSO
 
-L<String::Interpolate>, L<String::Interpolate::RE>, L<String::Expand>, L<String::MatchInterpolate>.
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2011 Smithsonian Astrophysical Observatory
-
-This is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-=head1 AUTHOR
-
-Diab Jerius
